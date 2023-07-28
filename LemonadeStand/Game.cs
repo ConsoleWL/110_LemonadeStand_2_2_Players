a9e5e5e35cb642a8ebcb700c4c1ef598bf8feaab
Nikita Obergan <Obergannikita@gmail.com>
2023-07-28 17:12:32 -0400

    Add method Enter number of players and Add method Generating players

--- LemonadeStand/Game.cs
+++ LemonadeStand/Game.cs
@@ -10,14 +10,15 @@ namespace LemonadeStand
     public class Game
     {
         int currentDay;
-        Player player;
+        int numberofPlayers;
+        List<Player> players;
         List<Day> days;
         Store store;
 
         public Game()
         {
             currentDay = 1;
-            player = new Player();
+            players = new List<Player>();
             store = new Store();
             days = new List<Day>
             {
@@ -29,8 +30,54 @@ namespace LemonadeStand
                 new Day(),
                 new Day()
             };
+            
         }
 
+        public int NumberOfPlayers()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter number of Players: 1, 2 , 3 , 4 , 5 :");
+
+                    numberofPlayers = Convert.ToInt32(Console.ReadLine());
+
+                    if(numberofPlayers > 0 && numberofPlayers <= 5)
+                    {
+                        return numberofPlayers;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nChoose beetween 1 and 5:");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nWrong input, must be an interger");
+                }
+            } 
+        }
+
+        public void GeneratePlayers(int numberOfPlayers)
+        {
+            for (int i = 1; i <= numberofPlayers; i++)
+            {
+                Console.WriteLine($"\nEnter a name for player {1}");
+                string name = Console.ReadLine();
+
+                if(name == "" || name == " " || name == "  ")
+                {
+                    players.Add(new Player());
+                    players[i -1].name = $"Player {i}";
+                }
+                else
+                {
+                    players.Add(new Player());
+                    players[i - 1].name = name;
+                }  
+            }
+        }
         public void Welcome()
         {
             Console.WriteLine("Welcome to Lemonade Stand!");
@@ -69,22 +116,22 @@ namespace LemonadeStand
             }
         }
 
-        public void CustomerPurchase()
-        {
-            for (int i = 0; i < days[currentDay - 1].customers.Count - 1; i++)
-            {
-                bool result = days[currentDay - 1].customers[i].Purchase(player, player.recipe, days[currentDay - 1].weather.condition);
+        //public void CustomerPurchase()
+        //{
+        //    for (int i = 0; i < days[currentDay - 1].customers.Count - 1; i++)
+        //    {
+        //        bool result = days[currentDay - 1].customers[i].Purchase(player, player.recipe, days[currentDay - 1].weather.condition);
 
-                if (result == true)
-                {
-                    player.Sell();
-                }
-                else
-                {
-                    Console.WriteLine("Customer pass by......");
-                }
-            }  
-        }
+        //        if (result == true)
+        //        {
+        //            player.Sell();
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Customer pass by......");
+        //        }
+        //    }  
+        //}
 
         public void DisplayActualWether()
         {
@@ -102,44 +149,47 @@ namespace LemonadeStand
             currentDay++;
         }
 
-        public void GameResuts()
-        {
-            Console.WriteLine($"\nThe week is over. Your total profit is {player.wallet.Money}");
-        }
+        //public void GameResuts()
+        //{
+        //    Console.WriteLine($"\nThe week is over. Your total profit is {player.wallet.Money}");
+        //}
         
-        public void GameSimulation()
-        {
-            while (currentDay < 8)
-            {
-                AnounceStartOftheDay();
+        //public void GameSimulation()
+        //{
+        //    while (currentDay < 8)
+        //    {
+        //        AnounceStartOftheDay();
 
-                days[currentDay - 1].weather.DisplayTemperature();
+        //        days[currentDay - 1].weather.DisplayTemperature();
 
-                player.OpenTheStand();
+        //        player.OpenTheStand();
 
-                store.DisplayStorePrices();
-                store.SellItems(player);
+        //        store.DisplayStorePrices();
+        //        store.SellItems(player);
 
 
-                player.DrinkPreperation();
+        //        player.DrinkPreperation();
 
-                WeatherChanger();
+        //        WeatherChanger();
 
-                CustomerPurchase();
+        //        CustomerPurchase();
 
-                DisplayActualWether();
+        //        DisplayActualWether();
 
-                player.CloseTheStand();
+        //        player.CloseTheStand();
 
-                AcounceEndOftheDay();
-            }
-        }
+        //        AcounceEndOftheDay();
+        //    }
+        //}
 
         public void RunGame()
         {
+            NumberOfPlayers();
+            GeneratePlayers(numberofPlayers);
+
             Welcome();
-            GameSimulation();
-            GameResuts();
+            //GameSimulation();
+            // GameResuts();
         }
 
     }
